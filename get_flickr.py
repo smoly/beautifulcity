f7db65f40e07604a8a91750e2f73829c514cb41c
Alexandra Smolyanskaya <alex.smoly@gmail.com>
2015-01-21 22:45:42 -0800

    improve API queries for flickr and instagram
    added exception handling and actually using the API libraries

--- get_flickr.py
+++ get_flickr.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import requests
 import csv
 import time
 from datetime import datetime
@@ -8,86 +7,76 @@ from pprint import pprint
 import flickrapi
 import json
 
+# Flickr API stuffs
+#TODO : move to local config
+api_key = '2a8751a5d69541e6c28667835c35cf11'
+api_secret = '6628ef02693fe943'
+flickr = flickrapi.FlickrAPI(api_key, api_secret)
+
+fieldnames = sorted(['datetaken', 'datetakengranularity', 'datetakenunknown', 'dateupload', 'latitude', 'longitude', 'owner', 'place_id', 'url_s', 'woeid'])
+
 def get_photos():
-   # query flickr api for photos with "#streetart" within SF
-
-    # list of working urls (on 1/17/15), having hard time forming correct api_sig
-    query_url = [
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=1&format=json&nojsoncallback=1&api_sig=7b56b8cfb62d621f702d8dafe19426cc',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=2&format=json&nojsoncallback=1&api_sig=429b354cbb1d2ba41dadcb60c30e8a8f',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=3&format=json&nojsoncallback=1&api_sig=1f4065555856ac808dfad336c85f993b',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=4&format=json&nojsoncallback=1&api_sig=7c7ca458e1cb624ca58a56b74e0aee42',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=5&format=json&nojsoncallback=1&api_sig=f1758f1fd269f17169986fb32b9ec801',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=6&format=json&nojsoncallback=1&api_sig=4c0d525052caf0f960fdc9fc818865cf',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=7&format=json&nojsoncallback=1&api_sig=47dae4d4aacfb2028689de5bcd3edd53',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=8&format=json&nojsoncallback=1&api_sig=f3c556b299b7f3da32bf516df5c74543',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=9&format=json&nojsoncallback=1&api_sig=f8a4f40859468c86b558e6319e315bf4',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=10&format=json&nojsoncallback=1&api_sig=2efd720b071e539f67c0a334ce28508d',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=11&format=json&nojsoncallback=1&api_sig=0d17c230242be60180a2bad7fcf7743f',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=12&format=json&nojsoncallback=1&api_sig=72af8edfd42bac0a56c24b4ccac2779b',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=13&format=json&nojsoncallback=1&api_sig=7fe4d8b1fe7277f458b7d30b03c641f0',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=14&format=json&nojsoncallback=1&api_sig=863e72595095bb37f5bc3c546f0eee05',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=15&format=json&nojsoncallback=1&api_sig=623724445f037d7c577190bc7d9947fb',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=16&format=json&nojsoncallback=1&api_sig=01cc0efa534ef5b00a2bc510f3e0f3f7',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=17&format=json&nojsoncallback=1&api_sig=dac6c0960a76dd0fd492cc5a2547ae67',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=18&format=json&nojsoncallback=1&api_sig=17c3c910799161b0c6719efdddb2c456',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=19&format=json&nojsoncallback=1&api_sig=2d392bda8f5b2ae6ca13cec3a6e4cfed',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=20&format=json&nojsoncallback=1&api_sig=2ce749258ca5add57ff408857c75e810',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=21&format=json&nojsoncallback=1&api_sig=02b018009372432a005f25c890f10932',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=22&format=json&nojsoncallback=1&api_sig=e6b68f75888e217d6fe171b8ebc6eb53',
-        'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f42fe846a36047efb1e75c42a8bf39a&tags=streetart&min_upload_date=1199145600&sort=date-posted-desc&has_geo=True&lat=37.774929500000000000&lon=-122.419415500000010000&radius=9&radius_units=km&extras=date_taken%2C+date_upload%2C+geo%2C+url_s&per_page=500&page=23&format=json&nojsoncallback=1&api_sig=c6f94588ad3c2079b3995237809e6d6b']
-
-
-    fieldnames = sorted(['datetaken', 'datetakengranularity', 'datetakenunknown', 'dateupload', 'latitude', 'longitude', 'owner', 'place_id', 'url_s', 'woeid'])
+   # query flickr api for photos with "#streetart, #graffiti, #mural, #tag, #publicart" within SF
 
     # get all pages of photo data
     req_page = 1
     now = datetime.now().isoformat().replace(':', '_') # mac doesn't allow ':' in filenames
-    with open('/Users/Alexandra/PycharmProjects/graphiti/data/flickr_data-%s.csv' % now, 'w') as output_file:
+    with open('/Users/Alexandra/Dropbox/Projects/Insight/tagsf/data/flickr_data-%s.csv' % now, 'w') as output_file:
 
         # write header
         # writer = csv.DictWriter(output_file, fieldnames=fieldnames, delimiter='\t')
         writer = csv.DictWriter(output_file, fieldnames=fieldnames)
         writer.writeheader()
 
-        for x in query_url:
+        req_page = 1
+        while True:
             time.sleep(1)
-            resp = requests.get(x)
-
-            # confirm page number match
-            if resp.json()['photos']['page'] != req_page:
-                print 'Requested page %i, got page %i' % (req_page, resp.json()['photos']['page'])
-                continue
+            resp = json.loads(flickr.photos.search(tags='streetart,graffiti,mural,tag,publicart',
+                     min_upload_date='1199145600',
+                     sort='date-posted-desc',
+                     has_geo='True',
+                     lat='37.774929500000000000',
+                     lon='-122.419415500000010000',
+                     radius='9',
+                     radius_units='km',
+                     extras='date_taken,date_upload,geo,url_s',
+                     per_page='500',
+                     page=req_page,
+                     format='json',
+                     nojsoncallback='1'))
 
             # loop through photos
-            for photo in resp.json()['photos']['photo']:
+            for photo in resp['photos']['photo']:
                 write_to_file(photo, fieldnames, writer)
 
-            print 'progress: page %i/%i complete' % (req_page, len(query_url))
-            req_page += 1
+            print 'progress: page %i/%i complete' % (resp['photos']['page'], resp['photos']['pages'])
+
+            if resp['photos']['page'] == resp['photos']['pages']:
+                break
+            else:
+                req_page += 1
+
 
 def write_to_file(photo, fieldnames, writer):
+    try:
+        relevant_data = {my_key: photo[my_key] for my_key in fieldnames}
+        writer.writerow(relevant_data)
+    except KeyError, e:
+        print 'KeyError, continuing: %s' % e
 
-    relevant_data = {my_key: photo[my_key] for my_key in fieldnames}
-    writer.writerow(relevant_data)
 
-def get_neighborhood():
+def get_neighborhood(timestamp):
+    # get_neighborhood(timestamp) where timestamp is timestamp of file to update
     # get neighborhood name from lat, long
     # Function written after flickr data was accessed, so we create a second cvs file here,
     #   adding a neighborhood column
-
-    # Flickr API stuffs
-    api_key = '2a8751a5d69541e6c28667835c35cf11'
-    api_secret = '6628ef02693fe943'
-    flickr = flickrapi.FlickrAPI(api_key, api_secret)
-
 
     # Working on reading, getting, and writing
     fieldnames_old = sorted(['datetaken', 'datetakengranularity', 'datetakenunknown', 'dateupload', 'latitude', 'longitude', 'owner', 'place_id', 'url_s', 'woeid'])
     fieldnames_new = fieldnames_old + ['neighborhood', 'place_type'] # + is immutable
 
-    with open('flickr_data-2015-01-16T19_24_33.067445.csv') as orig_f:
-        with open('flickr_data-2015-01-16T19_24_33.067445-neighborhood.csv', 'w') as new_f:
+    with open('flickr_data-%s.csv' % timestamp) as orig_f:
+        with open('flickr_data-%s-neighborhood.csv' % timestamp, 'w') as new_f:
             # orig_f = open('flickr_data-2015-01-16T19_24_33.067445.csv')
             # new_f = open('flickr_data-2015-01-16T19_24_33.067445-neighborhood.csv', 'w')
             start = time.time() # to have a start value for timing later
--- get_instagram.py
+++ get_instagram.py
@@ -1,75 +1,96 @@
-from instagram.client import InstagramAPI
-import sys
-import urllib
-import json
 import requests
 import csv
 import time
 from datetime import datetime
+import traceback
 
-
+# write list of posts to csv file, used below
 def read_write(result):
-    entries = 0
+    entries = [0, 0]
     for post in result['data']:
-        if post['location'] is not None:
-            if 'latitude' in post['location'].keys() \
-                    and 37.7 <= post['location']['latitude'] <= 37.85 \
-                    and -122.6 <= post['location']['longitude'] <= -122.35:
-                row_dict = {'user_id':  post['user']['id'],
-                    'image_url': post['images']['standard_resolution']['url'],
-                    'created_time': post['created_time'],
-                    'lat': post['location']['latitude'],
-                    'long': post['location']['longitude'],
-                    'post_url': post['link']
-                }
-                writer.writerow(row_dict)
-                entries += 1
-            # elif 'latitude' in post['location'].keys():
-            #     print "lat = %f, long = %f" % (post['location']['latitude'], post['location']['longitude'])
+        try:
+            if post['location'] is not None:
+                if 'latitude' in post['location'].keys(): # some are missing
+                    row_dict = {'user_id':  post['user']['id'],
+                        'image_url': post['images']['standard_resolution']['url'],
+                        'created_time': post['created_time'],
+                        'lat': post['location']['latitude'],
+                        'long': post['location']['longitude'],
+                        'post_url': post['link'],
+                        'id': post['id'],
+                        'likes': post['likes']['count']
+                    }
 
+                    # add text if available
+                    if 'caption' in post.keys() and post['caption'] is not None:
+                        if 'text' in post['caption'].keys():
+                            row_dict['text'] = post['caption']['text'].encode('ascii', 'replace')
+
+                    writer.writerow(row_dict)
+
+
+                    # count posts in SF and elsewhere
+                    if 37.7 <= post['location']['latitude'] <= 37.85 \
+                        and -122.6 <= post['location']['longitude'] <= -122.35:
+                        entries[0] += 1
+                    else:
+                        entries[1] += 1
+
+        except Exception, e:
+            print 'caught error in read_write, continuing: %s' % e
+            traceback.print_exc()
 
     return entries
 
 # first query
+#TODO: move to config
 start_time = time.time()
 access_token = '186061903.1fb234f.7e84aa71760b44cb9ab34f42a8ba00b0'
 query_url = 'https://api.instagram.com/v1/tags/streetart/media/recent?access_token=%s' % access_token
 
 # open file
-max_entries = 100000
-n_entries = 0
+max_entries = 10000.
+n_entries_SF = 0
+n_entries_all = 0
 
-fieldnames = sorted(['created_time', 'image_url', 'lat', 'long', 'user_id', 'post_url'])
+fieldnames = sorted(['id', 'likes', 'text', 'created_time', 'image_url', 'lat', 'long', 'user_id', 'post_url'])
 now = datetime.now().isoformat().replace(':', '_') # mac doesn't allow ':' in filenames
-with open('/Users/Alexandra/PycharmProjects/graphiti/data/instagram_data-%s.tsv' % now, 'w') as output_file:
+with open('/Users/Alexandra/Dropbox/Projects/Insight/tagsf/data/instagram_data_with_text_world-%s.tsv' % now, 'w') as output_file:
     writer = csv.DictWriter(output_file, fieldnames=fieldnames, delimiter='\t')
     writer.writeheader()
 
     # first query
+    start = time.time()
     resp = requests.get(query_url)
-    # if resp.status_code != 200:
-    #     # raise Exception(resp.status_code, resp.url, resp.text)
-    # else:
+
     new_entries = read_write(resp.json())
-    n_entries += new_entries
+    n_entries_SF += new_entries[0]
+    n_entries_all += new_entries[1]
 
     next_url = resp.json()['pagination']['next_url']
-    while n_entries < max_entries:
-        time.sleep(0.8)
-        resp = requests.get(next_url)
-        if resp.status_code != 200:
-            if resp.status_code == 500:
-                print resp.text
+    while True:
+        try:
+            # throttle to 3600/hour
+            end = time.time()
+            if (end-start) < 1:
+                time.sleep(end-start)
 
-            # else:
-                raise Exception(resp.status_code, resp.url, resp.text)
+            start = time.time()
+            resp = requests.get(next_url)
+            if resp.status_code != 200:
+                if resp.status_code == 500:
+                    print resp.text
+                # else:
+                    raise Exception(resp.status_code, resp.url, resp.text)
+            else:
+                new_entries = read_write(resp.json())
+                n_entries_SF += new_entries[0]
+                n_entries_all += new_entries[1]
 
-        else:
+                print 'progress: %i in SF, %i elsewhere, time elapsed = %3.fs' % (n_entries_SF, n_entries_all, time.time() - start_time)
 
-            new_entries = read_write(resp.json())
-            n_entries += new_entries
+                next_url = resp.json()['pagination']['next_url']
+        except Exception, e:
+            print 'caught error in body, continuing: %s' % e
+            traceback.print_exc()
 
-            print 'progress: %i/%i (%3.f percent), time elapsed = %3.fs' % (n_entries, max_entries, n_entries/max_entries, time.time() - start_time)
-
-            # print resp.json()['pagination']
-            next_url = resp.json()['pagination']['next_url']
