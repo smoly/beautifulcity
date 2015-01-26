717997cc8519c0c49fbeed10f4ab725cca75929e
Alexandra Smolyanskaya <alex.smoly@gmail.com>
2015-01-25 17:01:39 -0800

    add indexes on database

--- create_db_tagus.py
+++ create_db_tagus.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
+# reload CSV -> DATABASE, re-create indexes
+
 import config
 import pandas as pd
 import sqlalchemy
-from pandas.io import sql
 
 engine = sqlalchemy.create_engine('mysql://root@localhost') # connect to server
-
-# Select database, re-recreate table
 engine.execute('use tagus') # select new db
+
+# Define schema so as to not have pandas guess at types
 engine.execute('''
 drop table if exists instagram;
 create table instagram (
@@ -30,7 +31,6 @@ create table instagram (
 );
 ''')
 
-
 # Load Instagram data
 posts_all = pd.read_csv('%s/%s' % (config.data_path, config.instagram['file_name_world']), sep='\t')
 posts_all['date'] = pd.to_datetime(posts_all.created_time, unit='s')
@@ -38,8 +38,8 @@ posts_all['date'] = pd.to_datetime(posts_all.created_time, unit='s')
 # Kludge more date info:
 posts_all['date_year'] = posts_all['date'].apply(lambda x: str(x)[:4])
 posts_all['date_month'] = posts_all['date'].apply(lambda x: str(x)[:7])
-posts_all['date_day'] = posts_all['date'].apply(lambda x: str(x)[:10])
 posts_all['date_week'] = posts_all['date'].apply(lambda x: x.isocalendar()[1])
+posts_all['date_day'] = posts_all['date'].apply(lambda x: str(x)[:10])
 
 # Put into table
 posts_all.to_sql('instagram', engine,
@@ -49,10 +49,10 @@ posts_all.to_sql('instagram', engine,
                  index=False)
 
 # Create indices
-# create index idx_species_pk on sightings(species_pk);
-# create index idx_locations_pk on sightings(locations_pk); # 37 minutes
-# create index idx_observation_date on sightings(observation_date); # 14 minutes
-# create index idx_observers_pk on sightings(observers_pk); # TODO: build!!
-# create index idx_sei on sightings(sampling_event_identifier); # 30 minutes
-
-
+engine.execute('create index idx_lat_lng on instagram (lat, `long`);')
+engine.execute('create index idx_likes on instagram (likes)')
+engine.execute('create index idx_date on instagram (`date`)')
+engine.execute('create index idx_date_year on instagram (date_year)')
+engine.execute('create index idx_date_month on instagram (date_month)')
+engine.execute('create index idx_date_day on instagram (date_day)')
+engine.execute('create index idx_date_week on instagram (date_week)')
