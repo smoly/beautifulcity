b48367e5683a770e483f534d471c3e652cbbb0cc
Alexandra Smolyanskaya <alex.smoly@gmail.com>
2015-02-02 22:50:07 -0800

    removing unused example code

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request
 from app import app
-import pymysql as mdb
+# import pymysql as mdb
 from a_Model import ModelIt
 
-db= mdb.connect(user="root", host="localhost", db="world_innodb", charset='utf8')
+# db= mdb.connect(user="root", host="localhost", db="world_innodb", charset='utf8')
 
 
 @app.route('/')
@@ -14,30 +14,30 @@ def index():
        title = 'Home', user = {'nickname': 'Miguel' },
        )
 
-@app.route('/db')
-def cities_page():
-    with db:
-        cur = db.cursor()
-        cur.execute("SELECT Name FROM City LIMIT 15;")
-        query_results = cur.fetchall()
-    cities = ""
-    for result in query_results:
-        cities += result[0]
-        cities += "<br>"
-    return cities
-
-@app.route("/db_fancy")
-def cities_page_fancy():
-    with db:
-        cur = db.cursor()
-        cur.execute("SELECT Name, CountryCode,\
-            Population FROM City ORDER BY Population LIMIT 15;")
-
-        query_results = cur.fetchall()
-    cities = []
-    for result in query_results:
-        cities.append(dict(name=result[0], country=result[1], population=result[2]))
-    return render_template('cities.html', cities=cities)
+# @app.route('/db')
+# def cities_page():
+#     with db:
+#         cur = db.cursor()
+#         cur.execute("SELECT Name FROM City LIMIT 15;")
+#         query_results = cur.fetchall()
+#     cities = ""
+#     for result in query_results:
+#         cities += result[0]
+#         cities += "<br>"
+#     return cities
+#
+# @app.route("/db_fancy")
+# def cities_page_fancy():
+#     with db:
+#         cur = db.cursor()
+#         cur.execute("SELECT Name, CountryCode,\
+#             Population FROM City ORDER BY Population LIMIT 15;")
+#
+#         query_results = cur.fetchall()
+#     cities = []
+#     for result in query_results:
+#         cities.append(dict(name=result[0], country=result[1], population=result[2]))
+#     return render_template('cities.html', cities=cities)
 
 @app.route("/tag")
 def tag_home():
